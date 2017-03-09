5b5e9b68f6972e8e45bf3bc4a3c5960f83da8bb6
studente <studente@Liceospalla.local>
2017-03-09 11:56:28 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <string>
+#include <vector>
+using namespace std;
+void add_contact(rubrica) {
+}
+void print_contact(rubrica) {
+}
+void print_contact(rubrica) {
+}
+void print_contacts (rubrica) {
+}
+void get_index_off (rubrica) {
+}
+int main() {
+	
+}
+
+
